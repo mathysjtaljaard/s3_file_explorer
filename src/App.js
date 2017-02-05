3b9a55e7c4d0c66793ddf9ac1c1d01bc420cf0c3
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2017-02-05 09:15:42 -0500

    add click event

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
 class App extends Component {
+  selectedKey;
 
-  keys = ['key1','key2', 'key3'];
+  constructor(props) {
+    super(props);
+    this.state = {selected: false, selectedObject: ''};
+  }
 
-  keyData = [ {'Key':'key1', 'Data': 'some json data1'},
-              {'Key':'key2', 'Data': 'some json data2'},
-              {'Key':'key3', 'Data': 'some json data3'}];
   render() {
+
     return (
       <div className="App">
-
+        <div>
+          //Section to display lists
+          <ul>
+            {this.showkeys()}
+          </ul>
+        </div>
+        // section to display data
+        <div>
+          {this.state.selected && <DisplayData data={this.state.selectedObject}/>}
+        </div>
       </div>
     );
+  }
+
+  showkeys() {
+    var keys = ['key1', 'key2', 'key3'];
+
+    return keys.map((value) => {
+      return <li key={value}>
+        <a href="#" onClick={(e) => this.showKeyData(value)}>
+          {value}
+        </a></li>
+    });
+  }
+
+  showKeyData(value) {
+    var keyData = [
+      {'Key': 'key1', 'Data': {'data': 'some json data1'}},
+      {'Key': 'key2', 'Data': {'data': 'some json data1'}},
+      {'Key': 'key3', 'Data': {'data': 'some json data1'}}];
+
+
+    console.log('Pressed ', value);
+    return keyData.forEach((data) => {
+      if (data.Key === value) {
+        this.setState(prevState => ({
+          selected: true,
+          selectedObject: data
+        }));
+      }
+    });
+  }
+}
+
+class DisplayData extends React.Component {
+  render() {
+    return (<div>
+              <label>{this.props.data.Key}</label>
+              <textarea value={JSON.stringify(this.props.data.Data)} readOnly></textarea>
+            </div>);
   }
 }
 
